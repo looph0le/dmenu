c7cebb816368eea810a8881aae6182286353351e
looph0le <mitanshpanchal@gmail.com>
2022-11-04 21:58:00 +0530

    new

--- config.h
+++ config.h
@@ -4,19 +4,24 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"FantasqueSansMono Nerd Font:pixelsize=14"
+	"JetBrainsMono Nerd Font:pixelsize=14:weight=bold:antialias=true:autohint=true",
+	"JoyPixels:pixelsize=8:antialias=true:autohint=true"
 };
+static const unsigned int bgalpha = 0xe0;
+static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-
-#include"/home/looph0le/.cache/wal/colors-wal-dmenu.h"
-
-//static const char *colors[SchemeLast][2] = {
+static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-//	[SchemeNorm] = { "#bbbbbb", "#222222" },
-//	[SchemeSel] = { "#eeeeee", "#005577" },
-//	[SchemeOut] = { "#000000", "#00ffff" },
-//};
-
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
+static const unsigned int alphas[SchemeLast][2] = {
+	/*		fgalpha		bgalphga	*/
+	[SchemeNorm] = { fgalpha, bgalpha },
+	[SchemeSel] = { fgalpha, bgalpha },
+	[SchemeOut] = { fgalpha, bgalpha },
+};
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
@@ -26,3 +31,4 @@ static unsigned int lines      = 0;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
